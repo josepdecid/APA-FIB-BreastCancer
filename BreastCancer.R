5c544e00620c4ba92154041462cb5137b339dcff
JosepRIvaille <josep.de.cid@gmail.com>
2018-01-02 01:47:06 +0100

    Add RandomForest (not working)

--- BreastCancer.R
+++ BreastCancer.R
@@ -6,13 +6,19 @@
 # Q1 2017-2018
 ####################################################################
 
+library(corrplot)
+library(caTools)
+library(ggplot2)
+library(randomForest)
+library(caret)
+
 ####################################################################
 # SECTION 1: Data Preprocessing
 ####################################################################
 
 dataset <- read.csv('data.csv')
 # Remove unnecessary column 'X'
-dataset <- subset(dataset, select = -X )
+dataset <- subset(dataset, select = -c(X, id))
 
 summary(dataset)
 # We see that there are no NA values.
@@ -23,21 +29,20 @@ diagnosis <- as.factor(dataset$diagnosis)
 dataset$diagnosis <- diagnosis
 table(dataset$diagnosis) # Unbalanced observations
 
-# Feature Scaling ????
-
 # Take a look at heavy corrlations among different variables
-library(corrplot)
-correlation <- cor(dataset[,3:32])
+correlation <- cor(dataset[, 2:31])
 corrplot(correlation, order = 'hclust', tl.cex = 1, addrect = 8)
 
 # Split dataset into training and test set
-library(caTools)
-split = sample.split(dataset$id, SplitRatio = 0.8)
-training_set = subset(dataset, split = TRUE)
-test_set = subset(dataset, split = FALSE)
+split = sample.split(dataset$diagnosis, SplitRatio = 0.8)
+trainingSet = subset(dataset, split = TRUE)
+testSet = subset(dataset, split = FALSE)
+
+# Feature Scaling
+trainingSet[, 2:31] <- scale(trainingSet[, 2:31])
 
 # As we have a lot of dimensions and very correlated data, let's apply a PCA
-pca <- prcomp(dataset[, 3:32], center = TRUE, scale = TRUE)
+pca <- prcomp(dataset[, 2:31], center = TRUE, scale = TRUE)
 plot(pca, type="l")
 
 # Take a look at the variance proportion given by each component
@@ -48,7 +53,17 @@ summary(pca)
 # We would need 20 extra components only to obtain an extra 0.05, so we discard this ones.
 pca.df <- as.data.frame(pca$x)
 
-library(ggplot2)
 ggplot(pca.df) +
   geom_point(aes(x = PC1, y = PC2, col = diagnosis)) +
   ggtitle('Diagnosis distribution over first two Principal Components')
+
+####################################################################
+# SECTION 2: Model Building
+####################################################################
+
+# Random Forest Regression
+regressor.rf <- randomForest(formula = diagnosis ~ .,
+                             data = trainingSet,
+                             ntree = 100)
+pred.rf <- predict(regressor.rf, newdata = testSet)
+(conf.rf <- confusionMatrix(pred.rf, testSet$diagnosis, positive = 'M'))
