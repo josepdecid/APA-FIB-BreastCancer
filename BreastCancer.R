a07723e5aac7ead85f77d26d28577b3b2a5f8edd
JosepRIvaille <josep.de.cid@gmail.com>
2018-01-02 19:48:56 +0100

    Implement SVM

--- BreastCancer.R
+++ BreastCancer.R
@@ -1,7 +1,7 @@
 ####################################################################
 # APA Pràctica (Grau FIB)
 # Josep de Cid & Gonzalo Recio
-
+#
 # Breast Cancer Diagnostic
 # Q1 2017-2018
 ####################################################################
@@ -12,19 +12,7 @@
 # install.packages('randomForest') 
 # install.packages('caret')
 # install.packages('e1071')
-# install.packages('klaR')
-
-library(corrplot)
-library(caTools)
-library(ggplot2)
-library(randomForest)
-library(MASS)
-library(e1071)
-library(klaR)
-library(class)
-library(nnet)
-
-set.seed(422)
+set.seed(42)
 
 ####################################################################
 # SECTION 1: Data Preprocessing
@@ -44,10 +32,12 @@ dataset$diagnosis <- diagnosis
 table(dataset$diagnosis)  # Unbalanced observations
 
 # Take a look at heavy corrlations among different variables
+library(corrplot)
 correlation <- cor(dataset[, 2:31])
 corrplot(correlation, order = 'hclust', tl.cex = 1, addrect = 8)
 
 # Split dataset into training and test set
+library(caTools)
 split = sample.split(dataset$diagnosis, SplitRatio = 0.8)
 training.set = subset(dataset, split == TRUE)
 test.set = subset(dataset, split == FALSE)
@@ -68,6 +58,7 @@ summary(pca)
 # We would need 20 extra components only to obtain an extra 0.05, so we discard this ones.
 pca.df <- as.data.frame(pca$x)
 
+library(ggplot2)
 ggplot(pca.df) +
   geom_point(aes(x = PC1, y = PC2, col = diagnosis)) +
   ggtitle('Diagnosis distribution over first two Principal Components')
@@ -76,24 +67,8 @@ ggplot(pca.df) +
 # SECTION 2: Model Building
 ####################################################################
 
-### Random Forest Regression
-regressor.rf <- randomForest(formula = diagnosis ~ ., 
-                             data = training.set,
-                             importance = TRUE,  # Most significant variables
-                             ntree = 100)
-
-pred.rf <- predict(regressor.rf, newdata = test.set)
-
-# Random Forest Confusion matrix, Accuracy and Cross-Validation
-(conf.rf <- table(test.set[, 1], pred.rf))
-(acc.rf <- (conf.rf[1, 1] + conf.rf[2, 2]) / dim(test.set)[1])
-(cv.rf = rfcv(trainx = dataset[-1],
-              trainy = dataset$diagnosis,
-              cv.fold = 5,
-              scale="log", 
-              step=0.5))
-
 ### K-NN
+library(class)
 pred.knn <- knn(train = training.set[, -1],
                 test = test.set[, -1],
                 cl = training.set[, 1],
@@ -117,18 +92,54 @@ pred.log <- ifelse(prob.log > 0.5, 'M', 'B')
 (conf.log <- table(test.set[, 1], pred.log))
 (acc.log <- (conf.log[1, 1] + conf.log[2, 2]) / dim(test.set)[1])
 
+### Decision Tree
+
+
+### Random Forest Regression
+library(randomForest)
+regressor.rf <- randomForest(formula = diagnosis ~ ., 
+                             data = training.set,
+                             importance = TRUE,  # Most significant variables
+                             ntree = 100)
+
+pred.rf <- predict(regressor.rf, newdata = test.set)
+
+# Random Forest Confusion matrix, Accuracy and Cross-Validation
+(conf.rf <- table(test.set[, 1], pred.rf))
+(acc.rf <- (conf.rf[1, 1] + conf.rf[2, 2]) / dim(test.set)[1])
+(cv.rf = rfcv(trainx = dataset[-1],
+              trainy = dataset$diagnosis,
+              cv.fold = 5, scale = 'log', step = 0.5))
+
 ### SVM
+library(e1071)
+classifier.svm <- svm(formula = diagnosis ~ .,
+                      data = training.set,
+                      type = 'C-classification',
+                      kernel = 'linear')
+
+pred.svm <- predict(classifier.svm, newdata = test.set[-1])
+
+# SVM Confusion matrix and accuracy
+(conf.svm <- table(test.set[, 1], pred.svm))
+(acc.svm <- (conf.svm[1, 1] + conf.svm[2, 2]) / dim(test.set)[1])
 
 ### Naive Bayes
-model.nb <- naiveBayes(diagnosis ~ ., data = training.set)
+model.nb <- naiveBayes(diagnosis ~ .,
+                       data = training.set)
+
 pred.nb <- predict(model.nb, newdata = test.set)
+
+# Naive Bayes Confusion matrix and accuracy
 (conf.nb <- table(pred.nb, test.set$diagnosis))
 (acc.nb <- (conf.nb[1, 1] + conf.nb[2, 2]) / dim(test.set)[1])
-
 
 # we obtain 87% accuracy, let's try with cross-validation method for training the model
 
 #-------------- Dona el putu mateix-------------------------
+library(MASS)
+library(caret)
+library(klaR)
 train_control <- trainControl(method = 'LOOCV') #method="repeatedcv", number=10, repeats=3) # method="cv", number=10)
 #create model
 model.nbcv <- train(form = diagnosis ~ ., data = training.set, method = "nb", trControl=train_control)
@@ -139,9 +150,11 @@ pred.nbcv <- predict(model.nbcv, newdata = test.set)
 #------------------------------------------------------------
 
 ### Neural Networks
-model.nnet <- nnet(diagnosis ~ ., data=training.set, size=3, maxit=500, decay=0)
-pred.nnet <- as.factor(predict (model.nnet, newdata = test.set, type = 'class'))
-(conf.nnet <- table(pred.nnet, test.set$diagnosis))
-(acc.nnet <- (conf.nnet[1, 1] + conf.nnet[2, 2]) / dim(test.set)[1])
+library(nnet)
+classifier.nn <- nnet(diagnosis ~ .,
+                      data = training.set,
+                      size = 3, maxit = 500, decay = 0)
 
-### Decision Tree
+pred.nn <- as.factor(predict(classifier.nn, newdata = test.set, type = 'class'))
+(conf.nn <- table(pred.nn, test.set$diagnosis))
+(acc.nn <- (conf.nn[1, 1] + conf.nn[2, 2]) / dim(test.set)[1])
